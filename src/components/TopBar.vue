7b4b9e73d801f4c0d58725614cb673f99ba49a52
kibzrael <kibraphael7@gmail.com>
2022-08-10 14:01:53 +0300

    toggle theme

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="topbar | row">
     <h1>TODO</h1>
-    <img src="/icon-moon.svg" alt="Toggle Theme" />
+    <img
+      @click="toggleTheme"
+      :src="isDark ? '/icon-sun.svg' : '/icon-moon.svg'"
+      alt="Toggle Theme"
+    />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+let body = document.querySelector("body");
+let isDark = ref(body.classList.contains("dark"));
+
+function toggleTheme() {
+  isDark.value
+    ? body.classList.replace("dark", "light")
+    : body.classList.replace("light", "dark");
+  isDark.value = body.classList.contains("dark");
+  localStorage.setItem("theme", isDark.value ? "dark" : "light");
+}
+</script>
 <style lang=""></style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+let theme = localStorage.getItem('theme');
+
+if (theme!=null){
+    document.querySelector('body').className = theme;
+}
+
 createApp(App).mount('#app')
